219e2aada7c680f66e127907671e44889eac0cee
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2021-03-18 17:18:22 +0700

    add leaderboard feature. now able to load realtime data

---
+++ src/views/Leaderboard.vue
@@ -0,0 +1,203 @@
+<template>
+<div class="container-fluid" style="margin-top:100px">
+  <div class="row">
+        <div class="col-md-12">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Leaderboard</h6>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                      <div class="col-4">
+                        <label for="selectGame">Choose Game</label>
+                        <select class="form-control mb-2 mr-sm-3" id="selectGame" v-model="gameId">
+                              <!-- <option value="" disabled selected>Pilih Game</option> -->
+                              <option :value="game.gameId" v-for="(game, index) in games" :key="index">{{game.gameTitle}}</option>
+                        </select>
+                        </div>
+                        <div class="col-2">
+                        <label for="selectDate">Sort By</label>
+                        <select class="form-control mb-2 mr-sm-3" id="selectDate" v-model="sort">
+                              <!-- <option value="" disabled selected>Urutkan Berdasarkan</option> -->
+                              <option :value="sort" v-for="(sort, index) in sortlist" :key="index">{{sort.toUpperCase()}}</option>
+                        </select>
+                        </div>
+                        <!-- <form class="form-inline">
+                            <label class="sr-only" for="inlineFormInputName2">Pilih Game</label>
+                            <select class="form-control mb-2 mr-sm-3" id="inlineFormInputName2" v-model="gameId">
+                                  <option value="" disabled selected>Pilih Game</option>
+                                  <option :value="game.gameId" v-for="(game, index) in games" :key="index">{{game.gameTitle}}</option>
+                            </select>
+                            <select class="form-control mb-2 mr-sm-3" id="inlineFormInputName2" v-model="sort">
+                                  <option value="" disabled selected>Urutkan Berdasarkan</option>
+                                  <option :value="sort" v-for="(sort, index) in sortlist" :key="index">{{sort.toUpperCase()}}</option>
+                            </select>
+                        </form> -->
+                        <!-- <form action="" class="form-inline" >
+                            <div class="col">
+                                <label for="selectGame">Pilih Game</label>
+                                <select name="" id="selectGame" v-model="gameId" class="form-control">
+                                    <option value="" disabled selected>Pilih Game</option>
+                                    <option :value="game.gameId" v-for="(game, index) in games" :key="index">{{game.gameTitle}}</option>
+                                </select>
+                            </div>
+                            <div class="col">
+                                <label for="selectGame">Urutkan Berdasarkan</label>
+                                <select name="" id="selectGame" v-model="sort" class="form-control">
+                                    <option value="" disabled selected>Pilih Game</option>
+                                    <option :value="sort" v-for="(sort, index) in sortlist" :key="index">{{sort}}</option>
+                                </select>
+                            </div>
+                        </form> -->
+                        <!-- <form>
+                                <div class="col-2">
+                            <div class="col-auto my-1">
+                              <label class="mr-sm-2" for="inlineFormCustomSelect">Preference</label>
+                              <select class="form-control mb-2 mr-sm-3" id="inlineFormCustomSelect">
+                                <option selected>Choose...</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                              </select>
+                            </div>
+                            </div>
+                        </form> -->
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 mt-3">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>No.</th>
+                                            <th>Team Name</th>
+                                            <th>Leader Name</th>
+                                            <th>Game Data</th>
+                                            <th>Score</th>
+                                            <th>Date</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                      <!-- <tr v-for="(leaderboard, index) in leaderboard" :key="index"> 
+                                        <td>{{leaderboard}}</td> -->
+                                      <tr v-for="(leaderboard, index) in leaderboard" :key="index">
+                                          <td>{{index+1}}</td>
+                                          <td>
+                                            <span v-if="leaderboard.teamName">
+                                              <span>{{leaderboard.teamName}}</span>
+                                            </span>
+                                            <span v-else>-</span>
+                                          </td>
+                                          <td>{{leaderboard.leaderName}}</td>
+                                          <td>{{leaderboard.gameData.gameTitle}}</td>
+                                          <td>{{leaderboard.score}}</td>
+                                          <td>{{leaderboard.createdAt | formatDate}}</td>
+                                      </tr>
+                                      <!-- </tr> -->
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- ONLY FOR DEVELOPING -->
+              <!-- <div class="card bg-light">
+                <div class="card-header"> <h3>Games</h3> </div>
+                  <div class="card-inner">
+                    <div class="card bg-dark">
+                      <div class="card-inner bg-dark">
+                        <pre class="text-warning">{{games}}</pre>
+                      </div>
+                    </div>
+                </div>
+              </div> -->
+
+              <!-- <div class="card bg-light">
+                <div class="card-header"> <h3>Game ID & Sort By</h3> </div>
+                  <div class="card-inner">
+                    <div class="card bg-dark">
+                      <div class="card-inner bg-dark">
+                        <pre class="text-warning">{{gameId}} SORTED BY {{sort}}</pre>
+                      </div>
+                    </div>
+                </div>
+              </div>
+              <div class="card bg-light">
+                <div class="card-header"> <h3>Leaderboard</h3> </div>
+                  <div class="card-inner">
+                    <div class="card bg-dark">
+                      <div class="card-inner bg-dark">
+                        <pre class="text-warning">{{getLeaderboard}}</pre>
+                      </div>
+                    </div>
+                </div>
+              </div> -->
+              {{getLeaderboard}}
+              <!-- ONLY FOR DEVELOPING -->
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            leaderboard: '',
+            gameId: '',
+            sortlist: ['score', 'date'],
+            sort: '',
+            games: '',
+        }
+    },
+    methods: {
+      getGames() {
+        this.axios.get('game/mygame').then(response => {
+          this.games = response.data.data
+          // if (this.gameId == '') {
+          // this.gameId = this.games[0].gameId
+          // this.sort = this.sortlist[0]
+        // }
+          localStorage.setItem('gameId', this.games[0].gameId)
+          localStorage.setItem('sort', this.sortlist[0])
+        })
+      },
+      // getLeaderboard1() {
+      //   if (!this.gameId && !this.sort) {
+      //     this.gameId = localStorage.getItem('gameId')
+      //     this.sort = localStorage.getItem('sort')
+      //   }
+      //   this.axios.get('gameplay/leaderboard/game/'+this.gameId+'/sort/'+this.sort).then(response => {
+      //       this.leaderboard = response.data.data
+      //       console.log(response.data.message)
+      //   })
+      // },
+    },
+    created() {
+      this.getGames()
+    },
+    mounted() {
+      this.getLeaderboard
+    },
+    computed: {
+        getLeaderboard() {
+          if (!this.gameId && !this.sort) { 
+            this.gameId = localStorage.getItem('gameId')
+            this.sort = localStorage.getItem('sort')
+          }
+        this.axios.get(`gameplay/leaderboard/game/${this.gameId}/sort/${this.sort}`).then(response => {
+          if (this.sort == 'date') {
+            this.leaderboard = response.data.data.reverse()
+          } else {
+            this.leaderboard = response.data.data
+          }
+        })
+        }
+    }
+}
+</script>
+
+<style>
+</style>
